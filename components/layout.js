ca96fbda162e8b6d5589e206f3526223a1e66537
bilk0h <bil.koh.sec@gmail.com>
2020-06-08 12:30:28 -0700

    bio revision.

--- components/layout.js
+++ components/layout.js
@@ -25,7 +25,7 @@ export default function Layout({ children, home }) {
           <section className="aboutme">
             <h1>Stranger TTYs</h1>
             <p>
-              <u>My name is bilk0h.</u> I was a seventeen year old hacker in the year 2000. I liked 0day, defacing websites, and dropping docs on my irc rivals. I liked power. I liked it too much. One night, I stumbled upon a strange tty of unknown origins. Upon privilege escalation, my consciousness was sucked into an unknown cyberspace and I was disconnected from my body and lost to oblivion. But somehow I woke up, lost and confused, in the body of a thirty-six year old man in the year 2020. Now I'm on a mission to get back to the terminal where all this started. With my snarky robot sidekick, and a lot of catching up to do, I will stop at nothing.
+              <u>My name is bilk0h.</u> I was a seventeen year old hacker in the year 2000. I liked 0day, vulnerable IIS server, and competing with my irc rivals. I liked power. I liked it too much. One night, I stumbled upon a strange tty of unknown origins. Upon privilege escalation, my consciousness was sucked into an unknown cyberspace and I was disconnected from my body and lost to oblivion. But somehow I woke up, lost and confused, in the body of a thirty-six year old man in the year 2020. Now I'm on a mission to get back to the terminal where all this started. With my snarky robot sidekick, and a lot of catching up to do, I will stop at nothing.
               </p>
           </section>
         </aside>
