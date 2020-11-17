65cfe524bec52c3bd1014425fe374f38234287a5
bilk0h <bil.koh.sec@gmail.com>
2020-11-17 15:15:53 -0800

    Moved <aside> to bottom. SEO issue.

--- components/layout.js
+++ components/layout.js
@@ -21,15 +21,6 @@ export default function Layout({ children, home }) {
         <script async src="//static.getclicky.com/js"></script>
       </Head>
       <section className="wrapper">
-        <aside>
-          <section className="aboutme">
-            <h1>Stranger TTYs</h1>
-            <p>
-              <u>My name is bilk0h.</u> I was a seventeen year old hacker in the year 2000. I liked 0day, vulnerable IIS server, and competing with my irc rivals. I liked power. I liked it too much. One night, I stumbled upon a strange tty of unknown origins. Upon privilege escalation, my consciousness was sucked into an unknown cyberspace and I was disconnected from my body and lost to oblivion. But somehow I woke up, lost and confused, in the body of a thirty-six year old man in the year 2020. Now I'm on a mission to get back to the terminal where all this started. With my snarky robot sidekick, and a lot of catching up to do, I will stop at nothing.
-              </p>
-          </section>
-        </aside>
-
         <main className="content">
           <header>
             <ul className="nav">
@@ -51,6 +42,14 @@ export default function Layout({ children, home }) {
             </div>
           )}
         </main>
+        <aside>
+          <section className="aboutme">
+            <h1>Stranger TTYs</h1>
+            <p>
+              <u>My name is bilk0h.</u> I was a seventeen year old hacker in the year 2000. I liked 0day, vulnerable IIS server, and competing with my irc rivals. I liked power. I liked it too much. One night, I stumbled upon a strange tty of unknown origins. Upon privilege escalation, my consciousness was sucked into an unknown cyberspace and I was disconnected from my body and lost to oblivion. But somehow I woke up, lost and confused, in the body of a thirty-six year old man in the year 2020. Now I'm on a mission to get back to the terminal where all this started. With my snarky robot sidekick, and a lot of catching up to do, I will stop at nothing.
+              </p>
+          </section>
+        </aside>
       </section>
     </>
   )
